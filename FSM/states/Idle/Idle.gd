02b325291a11af2331505bf7018fcea86d017615
Kevin Murphy <kmurphy@etsy.com>
2018-07-20 12:26:55 -0700

    AutoCommit at 20/07/2018 12:26:55

--- FSM/states/Idle/Idle.gd
+++ FSM/states/Idle/Idle.gd
@@ -1,7 +1,7 @@
 extends "res://addons/moe.ero-one.fsm/content/FSMState.gd"
 
 func get_FSM(): return fsm; #defined in parent class
-func get_logic_root(): return logic_root; #defined in parent class
+func get_logic_root(): return logic_root; #defined in parent class 
 
 func state_init(args = null):
 	.state_init()
@@ -9,12 +9,10 @@ func state_init(args = null):
 #when entering state, usually you will want to reset internal state here somehow
 func enter(from_state = null, from_transition = null, args = []):
 	.enter(from_state, from_transition, args)
-	logic_root.show_idle()
 
 #when updating state, paramx can be used only if updating fsm manually
 func update(delta, args=null):
 	.update(delta, args)
-	logic_root.move(delta,Vector2(0,0))
 
 #when exiting state
 func exit(to_state=null):
