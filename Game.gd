1d2c5f1ea1c25479b2b4e7540a030a70a0179cf9
Kevin Murphy <kmurphy@etsy.com>
2018-07-20 13:18:44 -0700

    AutoCommit at 20/07/2018 13:18:44

--- Game.gd
+++ Game.gd
@@ -13,7 +13,7 @@ func _on_Boinger_boing(boing_vec,body):
 		body.vel = boing_vec
 
 func _on_MobSpawnTimer_timeout():
-	for i in range(randi() % 2):
+	for i in range(randi() % 1):
 		call_deferred("spawn_mob")
 
 func spawn_mob():
