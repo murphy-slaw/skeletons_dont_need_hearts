811a924375a3a74f85b7d54a1473b92c001babea
Kevin Murphy <kmurphy@etsy.com>
2018-07-20 12:04:40 -0700

    AutoCommit at 20/07/2018 12:04:40

--- Character.gd
+++ Character.gd
@@ -1,20 +1,38 @@
 extends KinematicBody2D
 
+export var walk_accel = 7
+export var friction = 0.03
+export var gravity = 9.8
+export var jump_power = -13
+var jump_speed = gravity * jump_power
+export var bounce = .99
+export var nudge_factor = 0.15
 
-var accel = 500
-var max_speed = 225
-var friction = -250
-var gravity = 500
-var jump_speed = -500
+var my_motion = Vector2()
 
+var flip_sprite = false setget set_flip_sprite, get_flip_sprite
 
-var bounce = .25
-
-var acc = Vector2()
-var motion = Vector2()
-var last_collider
+func get_flip_sprite():
+	return $Sprite.flip_h
 
 onready var ground_ray = get_node("ground_ray")
+onready var facing_ray = get_node("facing_ray")
+
+func set_flip_sprite(active=true):
+	$Sprite.flip_h = active
+	if $Sprite.flip_h:
+		$Sprite.offset = Vector2(2,0)
+		$facing_ray.global_rotation_degrees = 180
+	else:
+		$Sprite.offset = Vector2(0,0)
+		$facing_ray.global_rotation_degrees = 0
+
+
+func is_landed():
+	return is_on_floor() or ground_ray.is_colliding()
+
+func can_climb():
+	return false
 
 func _physics_process(delta):
 	var cur_accel = accel
