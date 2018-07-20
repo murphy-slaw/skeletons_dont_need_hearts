16783ee0b053b7ff1fa84bc59570f6f737fffb7e
Kevin Murphy <kmurphy@etsy.com>
2018-07-20 05:59:21 -0700

    AutoCommit at 20/07/2018 05:59:21

--- FSM/states/Climbing/Climbing.gd
+++ FSM/states/Climbing/Climbing.gd
@@ -13,7 +13,7 @@ func enter(from_state = null, from_transition = null, args = []):
 #when updating state, paramx can be used only if updating fsm manually
 func update(delta, args=null):
 	.update(delta, args)
-	var motion = Vector2(0,-10)
+	var motion = Vector2(0,-1)
 	logic_root.move(delta,motion)
 
 #when exiting state
