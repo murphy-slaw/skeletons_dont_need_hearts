53d67efe662180e5da824a7cc3d7f936ebf161c4
Kevin Murphy <kmurphy@etsy.com>
2018-07-20 06:32:05 -0700

    AutoCommit at 20/07/2018 06:32:05

--- FSM/states/Climbing/Climbing.gd
+++ FSM/states/Climbing/Climbing.gd
@@ -1,5 +1,7 @@
 extends "res://addons/moe.ero-one.fsm/content/FSMState.gd"
 
+var fatigue
+
 func get_FSM(): return fsm; #defined in parent class
 func get_logic_root(): return logic_root; #defined in parent class 
 
@@ -9,11 +11,14 @@ func state_init(args = null):
 #when entering state, usually you will want to reset internal state here somehow
 func enter(from_state = null, from_transition = null, args = []):
 	.enter(from_state, from_transition, args)
+	fatigue = 0
+
 
 #when updating state, paramx can be used only if updating fsm manually
 func update(delta, args=null):
 	.update(delta, args)
-	var motion = Vector2(0,-9.85)
+	var motion = Vector2(0,-9.85 + fatigue)
+	fatigue += .01
 
 	logic_root.my_motion.y = clamp(1,-1, logic_root.my_motion.y)
 	logic_root.move(delta,motion)
