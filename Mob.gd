f5d2af12cbbf2cdfada10f28fcff5c37fc776c03
Kevin Murphy <kmurphy@etsy.com>
2018-07-20 12:13:42 -0700

    AutoCommit at 20/07/2018 12:13:42

--- Mob.gd
+++ Mob.gd
@@ -6,16 +6,16 @@ func _ready():
 	if target == null:
 		target = self
 
-func get_acc_x():
-	var direction = (target.global_position - global_position).normalized()
-	acc.x = direction.x
-	acc.y += direction.y
-
-	
-func get_motion_y():
-	if (randi() % 30 == 1):
-        if is_on_floor():
-            motion.y = jump_speed
+#func get_acc_x():
+#	var direction = (target.global_position - global_position).normalized()
+#	acc.x = direction.x
+#	acc.y += direction.y
+#
+#	
+#func get_motion_y():
+#	if (randi() % 30 == 1):
+#        if is_on_floor():
+#            motion.y = jump_speed
 
 func _on_Lifespan_timeout():
 	queue_free()
