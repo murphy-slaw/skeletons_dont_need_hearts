ebe2eeea817ed5f26d59efaae7a0ddf1dce64c0e
Kevin Murphy <kmurphy@etsy.com>
2018-07-20 06:27:32 -0700

    AutoCommit at 20/07/2018 06:27:32

--- FSM/states/Climbing/Climbing.gd
+++ FSM/states/Climbing/Climbing.gd
@@ -14,8 +14,10 @@ func enter(from_state = null, from_transition = null, args = []):
 func update(delta, args=null):
 	.update(delta, args)
 	var motion = Vector2(0,-11)
-	logic_root.move(delta,motion)
+
 	logic_root.my_motion.y = clamp(5,-5, logic_root.my_motion.y)
+	logic_root.move(delta,motion)
+
 
 #when exiting state
 func exit(to_state=null):
