7c00a1108b8f455fe1bebc73755eafc38f32883c
Kevin Murphy <kmurphy@etsy.com>
2018-07-20 22:21:20 -0700

    AutoCommit at 20/07/2018 22:21:20

--- Player.gd
+++ Player.gd
@@ -1,14 +1,5 @@
 extends "res://Character.gd"
 
-func set_flip_sprite(active=true):
-	$Sprite.flip_h = active
-	if $Sprite.flip_h:
-		$Sprite.offset = Vector2(2,0)
-		$facing_ray.global_rotation_degrees = 180
-	else:
-		$Sprite.offset = Vector2(0,0)
-		$facing_ray.global_rotation_degrees = 0
-
 func can_climb():
 	return facing_ray.is_colliding()
 
