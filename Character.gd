fd4d3372736deb553a8041700141d0f009b5e91c
Kevin Murphy <kmurphy@etsy.com>
2018-07-20 21:21:12 -0700

    AutoCommit at 20/07/2018 21:21:12

--- Character.gd
+++ Character.gd
@@ -89,3 +89,15 @@ func show_jumping():
 
 func get_normalized_motion():
 	pass
+
+func get_circle_arc_poly(center, radius, angle_from, angle_to)
+    var nb_points = 32
+    var points_arc = PoolVector2Array()
+    points_arc.push_back(center)
+    var colors = PoolColorArray([color])
+
+    for i in range(nb_points+1):
+        var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
+        points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+    return points_arc
+
