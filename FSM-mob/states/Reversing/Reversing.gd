18f7400d2b75ee7a3cc838d4e2ecbf926f155ee1
Kevin Murphy <kmurphy@etsy.com>
2018-07-21 11:39:18 -0700

    AutoCommit at 21/07/2018 11:39:17

--- FSM-mob/states/Reversing/Reversing.gd
+++ FSM-mob/states/Reversing/Reversing.gd
@@ -14,6 +14,7 @@ func enter(from_state = null, from_transition = null, args = []):
 func update(delta, args=null):
 	.update(delta, args)
 	logic_root.flip_sprite = true
+	logic_root.my_motion.x = 0
 	var movement = Vector2(1,0)
 	if logic_root.get_flip_sprite():
 		movement = Vector2(-1,0)
