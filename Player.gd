07f8a77f33cc5b1cf7977acce5294da809a6198e
Kevin Murphy <kmurphy@etsy.com>
2018-07-20 22:28:38 -0700

    AutoCommit at 20/07/2018 22:28:38

--- Player.gd
+++ Player.gd
@@ -4,4 +4,5 @@ func can_climb():
 	return facing_ray.is_colliding()
 
 func get_normalized_motion():
-	return (int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")))
+	return (int(Input.is_action_pressed("ui_right"))\
+            - int(Input.is_action_pressed("ui_left")))
