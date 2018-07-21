1ed5ce32daf92275202001a7d37acb7d630bd067
Kevin Murphy <kmurphy@etsy.com>
2018-07-21 12:42:55 -0700

    AutoCommit at 21/07/2018 12:42:55

--- Main.gd
+++ Main.gd
@@ -9,9 +9,9 @@ var window_size = OS.get_window_size()
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	if OS.get_name()=="HTML5":
-		OS.set_window_maximized(true)
-	OS.set_window_position(screen_size*0.5 - window_size*0.5)
+#	if OS.get_name()=="HTML5":
+	OS.set_window_maximized(true)
+#	OS.set_window_position(screen_size*0.5 - window_size*0.5)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
