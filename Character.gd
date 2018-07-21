dbeb1a46a539f4a5fb0968ca1d530ccb2a56e327
Kevin Murphy <kmurphy@etsy.com>
2018-07-20 21:38:10 -0700

    AutoCommit at 20/07/2018 21:38:10

--- Character.gd
+++ Character.gd
@@ -23,7 +23,6 @@ func get_circle_arc_poly(center, radius, angle_from, angle_to):
     var nb_points = 32
     var points_arc = PoolVector2Array()
     points_arc.push_back(center)
-    var colors = PoolColorArray([color])
 
     for i in range(nb_points+1):
         var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
