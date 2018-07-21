050264ab88dec00ad029c71dcc6826d61a9639aa
Kevin Murphy <kmurphy@etsy.com>
2018-07-21 10:27:10 -0700

    AutoCommit at 21/07/2018 10:27:10

--- Game.gd
+++ Game.gd
@@ -13,9 +13,12 @@ func _on_Boinger_boing(boing_vec,body):
 		body.vel = boing_vec
 
 func _on_MobSpawnTimer_timeout():
-	call_deferred("spawn_mob")
+	for i in range(randi() % 2):
+		call_deferred("spawn_mob")
 
+	
 func spawn_mob():
+	$MobPath/MobSpawnLocation.set_offset(randi())
 	var mob = Mob.instance()
-	mob.position = $MobPath/MobSpawnLocation.position
 	call_deferred("add_child",mob)
+	mob.position = $MobPath/MobSpawnLocation.position
