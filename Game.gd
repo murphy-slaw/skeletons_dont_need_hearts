e74d169bbfec8d09b5fb3a4ca963b34e748df211
Kevin Murphy <kmurphy@etsy.com>
2018-07-22 22:15:41 -0400

    AutoCommit at 22/07/2018 22:15:41

--- Game.gd
+++ Game.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 export (PackedScene) var Mob
 
-var spawners = {}
-var used_spawners = {}
+var spawners = []
+var used_spawners = []
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	$MobSpawnTimer.start()
 	randomize()
-	get_tree().get_nodes_in_group
+	
+	spawners = get_tree().get_nodes_in_group("arches")
 
 func _on_Boinger_boing(boing_vec,body):
 	if body.get("vel") !=null:
@@ -19,6 +20,18 @@ func _on_Boinger_boing(boing_vec,body):
 func _on_MobSpawnTimer_timeout():
 	for i in range(randi() % 2):
 		call_deferred("spawn_mob")
+
+
+func get_spawn_point():
+	if spawners.size() == 0:
+		spawners = used_spawners.duplicate()
+		used_spawners = []
+	i = randi() % spawners.size()
+	spawner = spawners[i]
+	spawners.remove(i)
+	used_spawners.append(spawner)
+	return spawner/SpawnPoint
+	
 
 	
 func spawn_mob():
