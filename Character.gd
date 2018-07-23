62d0e197ec7c7fb7e7534522d48fd989ef5f5a77
Kevin Murphy <kmurphy@etsy.com>
2018-07-23 00:21:41 -0400

    AutoCommit at 23/07/2018 00:21:41

--- Character.gd
+++ Character.gd
@@ -64,7 +64,8 @@ func move(delta, the_move):
     # reduce the actual horizontal movement by
     # our current linear velocity * frictional constant
     if is_on_floor():
-        the_move.x -= my_motion.x * friction
+		pass
+#        the_move.x -= my_motion.x * friction
 
     # add the requested motion to our vector
     my_motion += the_move
