b12c1013543af3b8ba6e211bde79d60f339d2bf3
Kevin Murphy <kmurphy@etsy.com>
2018-07-23 19:45:32 -0400

    AutoCommit at 23/07/2018 19:45:32

--- Character.gd
+++ Character.gd
@@ -59,7 +59,7 @@ func can_climb():
 func move(delta, the_move):
 
     # always add gravity. it's good and good for you!
-    if not is_landed():
+    if not is_on_floor():
         my_motion.y += gravity
 
     # reduce the actual horizontal movement by
