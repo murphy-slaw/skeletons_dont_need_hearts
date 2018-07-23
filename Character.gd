7a25d2fc0ae9be5a9479c4d28bed5d7a0a43bc68
Kevin Murphy <kmurphy@etsy.com>
2018-07-23 18:54:07 -0400

    AutoCommit at 23/07/2018 18:54:07

--- Character.gd
+++ Character.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
-export var walk_accel = 7
-export var friction = 0.03
+export var walk_accel = 8
+export var friction = 0.07
 export var gravity = 9.8
-export var jump_power = -30
+export var jump_power = -25
 var jump_speed = gravity * jump_power
 export var bounce = 0
 export var nudge_factor = 0.15
