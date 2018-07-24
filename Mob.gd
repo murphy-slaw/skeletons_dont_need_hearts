700eaf7b3eb6bf29dbb6c75208aa67f968f07f7b
Kevin Murphy <kmurphy@etsy.com>
2018-07-23 23:51:35 -0400

    AutoCommit at 23/07/2018 23:51:35

--- Mob.gd
+++ Mob.gd
@@ -72,6 +72,12 @@ func _on_VisionArea_body_entered(body):
 func _on_VisionArea_body_exited(body):
     if body == target:
         target_in_range = false
+        
+        
+func overlaps_target():
+    if $VisionArea.overlaps_body(target):
+        target_in_range = true
+        return target_in_range
 
 func start_aggro():
     max_speed *= 5
