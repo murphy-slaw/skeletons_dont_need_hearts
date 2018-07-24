caf95e340b204db141d4f6e7907e294c7af72e15
Kevin Murphy <kmurphy@etsy.com>
2018-07-24 08:32:56 -0400

    AutoCommit at 24/07/2018 08:32:56

--- Mob.gd
+++ Mob.gd
@@ -14,9 +14,6 @@ export (int) var sight_radius = 150
 onready var target = get_parent().get_node("Player")
 onready var edge_ray = get_node("edge_ray")
 
-func _ready():
-    walk_accel = 3
-    max_speed = 100
 
 func get_circle_arc_poly(center, radius, angle_from, angle_to):
     var nb_points = 32
@@ -33,6 +30,8 @@ func _ready():
     $VisionArea/CollisionPolygon2D.polygon = circle_sector
     $VisionArea/CollisionPolygon2D.rotation_degrees = 90
     if target == null: target = self
+    walk_accel = 3
+    max_speed = 100
     original_max_speed = max_speed
     original_walk_accel = walk_accel
 
