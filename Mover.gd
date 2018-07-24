5456fa3f451bcafb6bc6f67d63ed798674d382d0
Kevin Murphy <kmurphy@etsy.com>
2018-07-23 23:18:43 -0400

    AutoCommit at 23/07/2018 23:18:43

--- Mover.gd
+++ Mover.gd
@@ -6,9 +6,9 @@ export var cycle = 1.0
 var accum = 0.0
 
 func _physics_process(delta):
-	accum += delta * (1.0 / cycle) * PI * 2.0
-	accum = fmod(accum, PI * 2.0)
-	var d = sin(accum)
-	var xf = Transform2D()
-	xf[2]= motion * d
-	$platform.transform = xf
+    accum += delta * (1.0 / cycle) * PI * 2.0
+    accum = fmod(accum, PI * 2.0)
+    var d = sin(accum)
+    var xf = Transform2D()
+    xf[2]= motion * d
+    $platform.transform = xf
