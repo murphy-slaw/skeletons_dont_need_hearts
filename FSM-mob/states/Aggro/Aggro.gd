f42da037595e160288c4972164c16effca7124b9
Kevin Murphy <kmurphy@etsy.com>
2018-07-23 20:59:29 -0400

    AutoCommit at 23/07/2018 20:59:29

--- FSM-mob/states/Aggro/Aggro.gd
+++ FSM-mob/states/Aggro/Aggro.gd
@@ -20,7 +20,7 @@ func update(delta, args=null):
     var movement = Vector2()
     movement.x= (logic_root.target.global_position - logic_root.global_position).normalized().x
     
-    logic_root.flip_sprite = (movement.x >= 1 and not logic_root.flip_sprite)
+    logic_root.flip_sprite = (movement.x)
 
     logic_root.walk(delta, movement)
 
