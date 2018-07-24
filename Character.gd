2e9d87bc817106e9cc325656cf9d95836df385ee
Kevin Murphy <kmurphy@etsy.com>
2018-07-24 08:08:31 -0400

    AutoCommit at 24/07/2018 08:08:31

--- Character.gd
+++ Character.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 export var walk_accel = 8
-export var bubba = -.05
+export var friction = -.003
 export var max_speed = 100
 export var gravity = 9.8
 export var jump_power = -25
@@ -45,9 +45,9 @@ func move(delta, acceleration):
     # reduce the actual horizontal movement by
     # our current linear velocity * frictional constant
     if acceleration.x == 0:
-        acceleration.x = bubba * velocity.x
+        acceleration.x = friction * velocity.x
 
-    $Label.text = str(acceleration) + str(velocity) + str(bubba)
+    $Label.text = str(acceleration) + str(velocity) + str(friction)
     # add the requested motion to our vector
     velocity += acceleration
 
