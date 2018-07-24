9d94cb1a57eb630fa0a29d915d74bad7dc6b8b20
Kevin Murphy <kmurphy@etsy.com>
2018-07-24 07:58:20 -0400

    AutoCommit at 24/07/2018 07:58:20

--- Character.gd
+++ Character.gd
@@ -64,7 +64,7 @@ func move(delta, acceleration):
     # object. Then we move and slide with our linear
     # velocity plus the floor velocity.
     var floor_vec = get_floor_velocity()
-#    velocity += floor_vec
+    velocity += floor_vec
     velocity = \
     move_and_slide(velocity, Vector2(0,-1))
 
@@ -72,7 +72,7 @@ func move(delta, acceleration):
     # movement vector, because otherwise we'll gradually
     # accelerate instead of just keeping pace with the thing
     # we're standing on!
-#    velocity -= floor_vec
+    velocity -= floor_vec
     
 func is_near_floor():
     var test_motion = Vector2(0,2)
