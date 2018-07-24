ad876297ca21fca2b5f7ce654ee496f149321030
Kevin Murphy <kmurphy@etsy.com>
2018-07-23 20:41:08 -0400

    AutoCommit at 23/07/2018 20:41:07

--- Mob.gd
+++ Mob.gd
@@ -25,18 +25,9 @@ func _ready():
     $VisionArea/CollisionPolygon2D.rotation_degrees = 90
     if target == null: target = self	
 
-#func get_acc_x():
-#	var direction = (target.global_position - global_position).normalized()
-#	acc.x = direction.x
-#	acc.y += direction.y
+
 #
 
-#func get_motion_y():
-#	if (randi() % 30 == 1):
-#        if is_on_floor():
-#            motion.y = jump_speed
-
-#onready var edge_ray = get_node("edge_ray")
 
 func _on_Lifespan_timeout():
     emit_signal("die")
@@ -45,8 +36,6 @@ func _on_Lifespan_timeout():
 func get_vision_normal():
     return ($facing_ray.position * $facing_ray.cast_to).normalized()
 
-#func get_normalized_motion():
-#	return Vector2(0,0)
     
 func check_ahead():
     var test_motion = Vector2(1,0)
