6f60f7d3bf66e5c2d653326403ec651cec9a7f56
Kevin Murphy <kmurphy@etsy.com>
2018-07-25 09:18:29 -0400

    AutoCommit at 25/07/2018 09:18:29

--- Main.gd
+++ Main.gd
@@ -17,3 +17,7 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func _on_Splash_start_game():
+    get_tree().change_scene("res://Game.tscn")
