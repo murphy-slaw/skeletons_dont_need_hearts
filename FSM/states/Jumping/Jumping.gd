be36ac24f89f16866dea45b51e665e847274c5f6
Kevin Murphy <kmurphy@etsy.com>
2018-07-25 19:41:34 -0400

    AutoCommit at 25/07/2018 19:41:34

--- FSM/states/Jumping/Jumping.gd
+++ FSM/states/Jumping/Jumping.gd
@@ -3,12 +3,17 @@ extends "res://addons/moe.ero-one.fsm/content/FSMState.gd"
 func get_FSM(): return fsm; #defined in parent class
 func get_logic_root(): return logic_root; #defined in parent class
 
+var vel 
+
 func state_init(args = null):
     .state_init()
 
 #when entering state, usually you will want to reset internal state here somehow
 func enter(from_state = null, from_transition = null, args = []):
+    vel = logic_root.jump_speed
     .enter(from_state, from_transition, args)
+    if Input.is_action_just_released("ui_up"):
+        vel *= .25
 
 #when updating state, paramx can be used only if updating fsm manually
 func update(delta, args=null):
