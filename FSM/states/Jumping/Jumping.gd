184a6b4f4e3ccbfead13a028a0940864ddf89747
Kevin Murphy <kmurphy@etsy.com>
2018-07-25 19:43:37 -0400

    AutoCommit at 25/07/2018 19:43:36

--- FSM/states/Jumping/Jumping.gd
+++ FSM/states/Jumping/Jumping.gd
@@ -12,13 +12,12 @@ func state_init(args = null):
 func enter(from_state = null, from_transition = null, args = []):
     vel = logic_root.jump_speed
     .enter(from_state, from_transition, args)
-    if not Input.is_action_pressed("ui_up"):
+    if not Input.is_action_just_pressed("ui_up"):
         vel *= .25
 
 #when updating state, paramx can be used only if updating fsm manually
 func update(delta, args=null):
     .update(delta, args)
-        
     var motion = Vector2(0,vel)
     logic_root.move(delta,motion)
 
