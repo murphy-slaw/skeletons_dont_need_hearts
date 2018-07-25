d8fb6c912dfb2d772f3db5aad10b8c73b8512806
Kevin Murphy <kmurphy@etsy.com>
2018-07-25 00:13:50 -0400

    AutoCommit at 25/07/2018 00:13:50

--- Player.gd
+++ Player.gd
@@ -8,7 +8,6 @@ func process_collisions():
     var count = get_slide_count()
     for i in range(count):
         var collision = get_slide_collision(i)
-        $Label.text = str(collision.collider)
         if collision.collider.has_method("hit"):
                 collision.collider.hit()
 
