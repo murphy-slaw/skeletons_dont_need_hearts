ff8ebe91d6051dd3e6402403c7a916b30494d367
Kevin Murphy <kmurphy@etsy.com>
2018-07-25 00:15:58 -0400

    AutoCommit at 25/07/2018 00:15:58

--- FSM-mob/states/Hitting/Hitting.gd
+++ FSM-mob/states/Hitting/Hitting.gd
@@ -3,18 +3,20 @@ extends "res://addons/moe.ero-one.fsm/content/FSMState.gd"
 func get_FSM(): return fsm; #defined in parent class
 func get_logic_root(): return logic_root; #defined in parent class 
 
+var source_state
+
 func state_init(args = null):
     .state_init()
 
 #when entering state, usually you will want to reset internal state here somehow
 func enter(from_state = null, from_transition = null, args = []):
     .enter(from_state, from_transition, args)
+    source_state = from_state
     logic_root.is_hit = false
     if not logic_root.can_see_target:
         logic_root.die()
     else:
         logic_root.target.hit()
-    exit(from_state)
 
 #when updating state, paramx can be used only if updating fsm manually
 func update(delta, args=null):
