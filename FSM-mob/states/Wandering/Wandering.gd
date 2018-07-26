99e51d89762e3bd082c9ad4317ff3b3e4c8c7f0a
Kevin Murphy <kmurphy@etsy.com>
2018-07-26 09:06:49 -0400

    AutoCommit at 26/07/2018 09:06:49

--- FSM-mob/states/Wandering/Wandering.gd
+++ FSM-mob/states/Wandering/Wandering.gd
@@ -4,16 +4,17 @@ func get_FSM(): return fsm; #defined in parent class
 func get_logic_root(): return logic_root; #defined in parent class 
 
 func state_init(args = null):
-	.state_init()
+    .state_init()
 
 #when entering state, usually you will want to reset internal state here somehow
 func enter(from_state = null, from_transition = null, args = []):
-	.enter(from_state, from_transition, args)
+    .enter(from_state, from_transition, args)
 
 #when updating state, paramx can be used only if updating fsm manually
 func update(delta, args=null):
-	.update(delta, args)
+    .update(delta, args)
+    logic_root.walk(delta, logic_root.facing_normal)
 
 #when exiting state
 func exit(to_state=null):
-	.exit(to_state)
+    .exit(to_state)
