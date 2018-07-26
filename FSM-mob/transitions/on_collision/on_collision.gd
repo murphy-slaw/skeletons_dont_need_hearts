7326f50353d8f9ab4a1cb4cbce15143332626bb2
Kevin Murphy <kmurphy@etsy.com>
2018-07-25 22:11:35 -0400

    AutoCommit at 25/07/2018 22:11:35

--- FSM-mob/transitions/on_collision/on_collision.gd
+++ FSM-mob/transitions/on_collision/on_collision.gd
@@ -5,13 +5,12 @@ func get_fsm(): return fsm; #access to owner FSM, defined in parent class
 func get_logic_root(): return logic_root; #access to logic root of FSM (usually fsm.get_parent())
 
 func transition_init(args = []): 
-	#you can optionally implement this to initialize transition on it's creation time 
-	pass
+    #you can optionally implement this to initialize transition on it's creation time 
+    pass
 
 func prepare(new_state, args = []): 
-	#you can optionally implement this to reset transition when related state has been activated
-	pass
+    #you can optionally implement this to reset transition when related state has been activated
+    pass
 
 func transition_condition(delta, args = []): 
-	# Return true/false
-	return false;
+    return logic_root.check_ahead() or logic_root.is_on_wall();
