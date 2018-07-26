c299851ad1f9bc47ab4a72f29a49b43a98f09ae8
Kevin Murphy <kmurphy@etsy.com>
2018-07-25 20:20:02 -0400

    AutoCommit at 25/07/2018 20:20:02

--- Player.gd
+++ Player.gd
@@ -2,6 +2,8 @@ extends "res://Character.gd"
 
 var hearts = 3
 
+func _ready():
+    enemy_layer = 1
 func get_normalized_motion():
     return (int(Input.is_action_pressed("ui_right"))\
             - int(Input.is_action_pressed("ui_left")))
