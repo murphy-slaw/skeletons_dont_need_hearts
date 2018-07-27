9c0cd2d074f65bdebfed0c5ea1ca2cfa2945c732
Kevin Murphy <kmurphy@etsy.com>
2018-07-27 10:58:19 -0400

    Fix mobs checking flags in Hitting state during enter() instead of update()

--- src/FSM-mob/states/Hitting/Hitting.gd
+++ src/FSM-mob/states/Hitting/Hitting.gd
@@ -10,16 +10,16 @@ func state_init(args = null):
 #when entering state, usually you will want to reset internal state here somehow
 func enter(from_state = null, from_transition = null, args = []):
     .enter(from_state, from_transition, args)
+
+#when updating state, paramx can be used only if updating fsm manually
+func update(delta, args=null):
+    .update(delta, args)
     if not logic_root.can_see_target:
         logic_root.is_dying = true
     else:
         logic_root.is_hit = false
         logic_root.target.hit(self)
         yield(get_tree().create_timer(2), "timeout");
-
-#when updating state, paramx can be used only if updating fsm manually
-func update(delta, args=null):
-    .update(delta, args)
 
 #when exiting state
 func exit(to_state=null):
