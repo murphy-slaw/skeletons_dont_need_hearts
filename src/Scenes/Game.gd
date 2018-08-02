3b4a93588ba5a42150f47aed15b348d78dc7d165
Kevin Murphy <kmurphy@etsy.com>
2018-08-01 18:50:15 -0700

    fixes for audio transitons

--- src/Scenes/Game.gd
+++ src/Scenes/Game.gd
@@ -1,5 +1,63 @@
 extends Node2D
 
+
+class AudioManager:
+    
+    var players
+    var locked = false
+    var tween
+    
+    func _init(p = [], t = Tween.new()):
+        players = p
+        tween = t
+        players[0].volume_db = 0
+        players[1].volume_db = -80
+        
+    func crossfade_audio(transition_duration = 1, 
+        transition_type = Tween.TRANS_SINE, 
+        ease_type = Tween.EASE_OUT):
+            
+        if locked:
+            print("waiting to crossfade due to lock")
+            yield(tween,"tween_completed")
+        else:
+            locked = true
+            
+        print("in crossfade_audio")
+        
+        assert players.size() == 2
+        
+        tween.interpolate_property(players[1], "volume_db",
+            players[1].volume_db, 0,
+            transition_duration, transition_type, ease_type, 0)
+        
+        tween.interpolate_property(players[0], "volume_db",
+            players[0].volume_db, -36,
+            transition_duration, transition_type, ease_type, 0)
+        tween.start()
+        
+        yield(tween, "tween_completed")
+        tween.remove_all()
+        
+        players.invert()        
+        locked = false
+
+    func play_active(stream):
+        players[0].stop()
+        players[0].stream = stream
+        players[0].play()
+        
+    func play_muted(stream):
+        players[1].stop()
+        players[1].stream = stream
+        players[1].play()
+        
+    func fade_active(duration, transition_type):
+            tween.interpolate_property(players[0], "volume_db", 
+            players[0].volume_db, -36, duration, transition_type,
+            transition_type, Tween.EASE_IN, 4)       
+        
+
 export (PackedScene) var Mob
 
 var spawners = []
@@ -7,6 +65,7 @@ var used_spawners = []
 var mobs = {}
 var max_mobs = 0
 var angry_mob_count = 0
+var last_aggro_state = false
 export (int) var win_hearts = 10
 
 var is_ending = false
@@ -15,15 +74,17 @@ onready var player = find_node("Player")
 onready var tween = get_node("Tween")
 onready var audio1 = get_node("MusicPlayerSneak")
 onready var audio2 = get_node("MusicPlayerFight")
-onready var active_audio = audio1
-onready var muted_audio = audio2
 
 var fail_tune = preload("res://audio/music/skelefailure.ogg")
 var win_tune = preload("res://audio/music/skelevictory.ogg")
+var audio_manager
+var audio_time = 0.0
 
 func _ready():
     modulate = Color(0,0,0,1)
     randomize()
+    
+    audio_manager = AudioManager.new([audio1, audio2], tween)
     
     spawners = get_tree().get_nodes_in_group("arches")
     max_mobs = spawners.size()
@@ -39,21 +100,19 @@ func _ready():
     var transition_type = Tween.TRANS_SINE
     var transition_duration = 2
     tween.interpolate_property(self, "modulate", Color(0,0,0,1), Color(1,1,1,1), transition_duration, transition_type, Tween.EASE_IN, 0)
-    audio2.stream = load("res://audio/music/skelestart.ogg")
-    audio2.play()
+   
+
+    audio_manager.play_active(load("res://audio/music/skelestart.ogg"))
     tween.start()
     get_tree().paused = true
     yield(tween,"tween_completed")
     
-    tween.interpolate_property(audio2, "volume_db",
-        audio2.volume_db, -36,
-        2, transition_type, Tween.EASE_IN, 0)
+    audio_manager.crossfade_audio()
     
     get_tree().paused = false
     yield(tween,"tween_completed")
-    audio2.stream = load("res://audio/music/skelefight.ogg")
-    audio2.play()
-    audio1.play()
+    audio_manager.play_muted(load("res://audio/music/skelefight.ogg"))
+    audio_manager.play_active(load("res://audio/music/skelesneak.ogg"))
     
 func _physics_process(delta):
         $UILayer/MarginContainer/TextureProgress.value = player.hearts
@@ -67,19 +126,27 @@ func _physics_process(delta):
                 player.win()
                 good_ending()
                 
+                
+func _process(delta):
+    audio_time += delta
+    if audio_time > 1:
+        audio_time = 0.0
+        var is_aggro = (angry_mob_count > 0)
+        if  is_aggro != last_aggro_state:
+            last_aggro_state = is_aggro
+            audio_manager.crossfade_audio(0.25)
+                
             
 func good_ending():
-    muted_audio.stop()
-    muted_audio.stream = win_tune
-    muted_audio.play()
-    crossfade_audio(0.25)
+    audio_manager.play_muted(win_tune)
+    audio_manager.crossfade_audio(0.25)
     yield(get_tree().create_timer(2.0),"timeout")
 
     var transition_type = tween.TRANS_SINE
     var transition_duration = 8
     tween.remove_all()
     tween.interpolate_property(self, "modulate", Color(1,1,1,1), Color(1,1,1,0), transition_duration, transition_type, Tween.EASE_IN, 4)
-    tween.interpolate_property(active_audio, "volume_db", active_audio.volume_db, -36, transition_duration, transition_type, Tween.EASE_IN, 4)
+    audio_manager.fade_active(transition_duration, transition_type)
     tween.start()
     get_tree().paused = true
     yield(tween,"tween_completed")
@@ -88,17 +155,15 @@ func good_ending():
     
     
 func bad_ending():
-    muted_audio.stop()
-    muted_audio.stream = fail_tune
-    muted_audio.play()
-    crossfade_audio(0.25)
+    
+    audio_manager.play_muted(fail_tune)
+    audio_manager.crossfade_audio(0.25)
     yield(player.animation_player,"animation_finished")
 
     var transition_type = tween.TRANS_SINE
     var transition_duration = 4
     tween.remove_all()
     tween.interpolate_property(self, "modulate", Color(1,1,1,1), Color(0,0,0,1), transition_duration, transition_type, Tween.EASE_IN, 4)
-    tween.interpolate_property(active_audio, "volume_db", active_audio.volume_db, -80, transition_duration, transition_type, Tween.EASE_IN, 4)
     tween.start()
     get_tree().paused = true
     yield(tween,"tween_completed")
@@ -151,47 +216,15 @@ func _on_mob_died(mob):
     if mob.get_state() == 'Aggro':
         print("on_mob_died, decrementing angry_mob_count")
         angry_mob_count -= 1
-        flip_audio_if_calm()
     mobs.erase(mob)
 
 func _on_mob_aggro():
-    print("on_mob_aggro, incrementing angry_mob_count")
-    print(angry_mob_count)
-    flip_audio_if_calm()
+    print("on_mob_aggro, incrementing angry_mob_count, current count" + str(angry_mob_count))
     angry_mob_count += 1
 
 func _on_mob_calm():
     print("on_mob_calm, decrementing angry_mob_count")
-    angry_mob_count -=1
-    flip_audio_if_calm()
-
-func flip_audio_if_calm():
-    if angry_mob_count == 0:
-        crossfade_audio(0.25) 
-
-func crossfade_audio(transition_duration = 1, 
-    transition_type = Tween.TRANS_SINE, 
-    ease_type = Tween.EASE_OUT):    
-    
-    tween.interpolate_property(muted_audio, "volume_db",
-        muted_audio.volume_db, 0,
-        transition_duration, transition_type, ease_type, 0)
-    
-    tween.interpolate_property(active_audio, "volume_db",
-        active_audio.volume_db, -36,
-        transition_duration, transition_type, ease_type, 0)
-    tween.start()
-    
-    yield(tween, "tween_completed")
-    tween.remove_all()
-    
-    if audio1.volume_db == 0:
-        active_audio = audio1
-        muted_audio = audio2
-    else:
-        active_audio = audio2
-        muted_audio = audio1
-    
+    angry_mob_count -=1    
 
 func _on_Player_hit(body):
     if body.is_class("Mob"):
